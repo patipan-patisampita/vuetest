c518ada4c4c2701d543a74c573b4d2ed28a8f11d
patipan-patisampita <patipan.patisampita@gmail.com>
2021-07-09 23:21:55 +0700

    third

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="เว็บไซต์" />
+    <h1>เว็บไซต์</h1>
+    <!-- <HelloWorld msg="เว็บไซต์" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 };
 </script>
