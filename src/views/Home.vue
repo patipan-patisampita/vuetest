3f3615149697232858827b22e9d729c0b787aa7a
patipan-patisampita <patipan.patisampita@gmail.com>
2021-07-09 23:15:27 +0700

    secound

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="เว็บไซต์" />
   </div>
 </template>
 
